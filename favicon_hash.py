7f64ab2480f8130c06cbdec10e4ad2686105f204
Whitehorse-rainbow <78192261+Whitehorse-rainbow@users.noreply.github.com>
2021-02-02 00:01:52 +0800

    Update favicon_hash.py

--- favicon_hash.py
+++ favicon_hash.py
@@ -5,3 +5,5 @@ favicon = response.content.encode('base64')
 hash = mmh3.hash(favicon)
 print 'http.favicon.hash:'+str(hash)
 
+#需要mmh3，microsoft visual c++ 9.0以上
+#https://download.microsoft.com/download/7/9/6/796EF2E4-801B-4FC4-AB28-B59FBF6D907B/VCForPython27.msi
